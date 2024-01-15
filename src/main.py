8bed4a6c95b0ddb1166ee8f8b9284ebab597a6b1
tyrosine-rex <thomas.lahaie@icloud.com>
2024-01-15 15:24:55 +0100

    update Tour of Grid class

--- src/main.py
+++ src/main.py
@@ -13,16 +13,47 @@ def main():
     final_size = deck.remain
 
     print(f"""
-        nombre de carte: {init_size}
+          
 
-        premier tirage: {first_card}
+    nombre de carte: {init_size}
 
-        second tirage: {second_card}
+    premier tirage: {first_card}
 
-        cinq prochains tirages: {next_five_cards}
+    second tirage: {second_card}
 
-        nombre de carte après les tirage: {final_size}
+    cinq prochains tirages: {next_five_cards}
+
+    nombre de carte après les tirage: {final_size}
+
+
     """)
+
+    board = Grid(5, 5)
+
+    print(f"Only the First Tile:\n{board}")
+
+    board._add_node(0, 1)    #Two Node = Two Tile = One Domino
+    board._add_node(0, 2)
+
+    print(f"With 1 domino:\n{board}")
+
+    board._add_node(1, 2)
+    board._add_node(2, 2)
+
+    print(f"With 2 dominos:\n{board}")
+
+    board._add_node(0, -1)
+    board._add_node(-1, -1)
+
+    print(f"With 3 dominos:\n{board}")
+
+    
+
+    print(" 'X' -> No Domino/Node Allowed")
+    print(" '.' -> Domino/Node Allowed")
+    print(" '#' -> A Domino/Node")
+    print(" 'O' -> Initial Domino")
+
 
 if __name__ == "__main__":
     main()
