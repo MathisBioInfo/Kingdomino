402f0fad408b38b7bf814a69f9ebfd51cbda6009
tyrosine-rex <thomas.lahaie@icloud.com>
2024-01-15 17:38:24 +0100

    show_free_places method

--- src/lib/board.py
+++ src/lib/board.py
@@ -17,6 +17,10 @@ def cartesian_product(list_1, list_2):
     return prod
 
 
+def flat_n_uniq(list):
+    return set([elmt for sublist in list for elmt in sublist])
+
+
 class GameBoard:
     def __init__(self, width, height):
         self.initial_bounds = Bounds(-width, width, -height, height)
@@ -28,14 +32,19 @@ class GameBoard:
         self._update_free_node_places(0, 0)
         self._update_free_domino_places()
 
-    def __repr__(self):
+    
+    def __repr__(self, show_free_places=False):
+        free_places = flat_n_uniq(self.free_domino_places) if show_free_places else {}
         repr = []
         for y in range(self.initial_bounds.max_y, self.initial_bounds.min_y-1, -1):
             repr.append(f"{y:^3}" + " " * 1)
             for x in range(self.initial_bounds.min_x, self.initial_bounds.max_x+1):
                 if self._is_not_overbounded_node(x, y):
-                    tile = self.nodes.get((x, y), Tile())
-                    repr.append(tile.__repr__())
+                    if (x, y) in free_places:
+                        repr.append(" F ")
+                    else:
+                        tile = self.nodes.get((x, y), Tile())
+                        repr.append(tile.__repr__())
                 else:
                     repr.append(" X ")
             repr.append("\n")
@@ -43,19 +52,28 @@ class GameBoard:
         repr.extend([f"{x:^3}" for x in range(self.initial_bounds.min_x, self.initial_bounds.max_x+1)])
         repr.append("\n")
         return "".join(repr)
+    
+    
+    def show_free_places(self):
+        print(self.__repr__(True))
 
+    
     def _get_max_x(self):
         return max([x for x, _ in self.nodes])
 
+    
     def _get_min_x(self):
         return min([x for x, _ in self.nodes])
 
+    
     def _get_max_y(self):
         return max([y for _, y in self.nodes])
 
+    
     def _get_min_y(self):
         return min([y for _, y in self.nodes])
 
+    
     def _update_bounds(self):
         self.current_bounds = Bounds(
             self.initial_bounds.min_x + self._get_max_x(),
@@ -64,10 +82,12 @@ class GameBoard:
             self.initial_bounds.max_y + self._get_min_y(),
         )
 
+    
     def _update_free_node_places(self, x, y):
         new_places = (self._free_node_places | set(self.get_free_neighbors_coords(x, y))) - {(x, y)}
         self._free_node_places = {p for p in new_places if self._is_not_overbounded_node(*p)}
 
+    
     def _update_free_domino_places(self):
         res = []
         for p in self._free_node_places:
@@ -79,6 +99,7 @@ class GameBoard:
 
         self.free_domino_places = set(res)
 
+    
     def _is_not_overbounded_node(self, x, y):
         return (
             x > self.current_bounds.min_x and
@@ -87,22 +108,26 @@ class GameBoard:
             y < self.current_bounds.max_y
         )
 
+    
     def _is_not_overbounded_domino(self, pos_1, pos_2):
         return (
             self._is_not_overbounded_node(*pos_1) and
             self._is_not_overbounded_node(*pos_2)
         )
 
+    
     def _add_node(self, x, y, tile):
         self.nodes[(x, y)] = tile
         self._update_bounds()
         self._update_free_node_places(x, y)
 
+    
     def _add_domino(self, pos_1, pos_2, tiles):
         self._add_node(*pos_1, tiles[0])
         self._add_node(*pos_2, tiles[1])
         self._update_free_domino_places()
 
+    
     def to_matrix(self):
         mat = []
         for x in range(self.current_bounds.min_x+1, self.current_bounds.max_x):
@@ -112,24 +137,31 @@ class GameBoard:
             mat.append(sub_mat)
         return mat
 
+    
     def to_adjacency_matrix(self):
         raise NotImplementedError
 
+    
     def to_adjacency_list(self):
         raise NotImplementedError
 
+    
     def get_neighbors_coords(self, x, y):
         return [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]
 
+    
     def get_free_neighbors_coords(self, x, y):
         return [p for p in self.get_neighbors_coords(x, y) if p not in self.nodes]
 
+    
     def get_neighbors(self, x, y):
         return {coord: self.nodes.get(coord, Tile()) for coord in self.get_neighbors_coords(x, y)}
 
+    
     def add_domino(self, pos_1, pos_2, tiles):
         if pos_1 not in self.get_neighbors(*pos_2):
-            raise Exception("invalid domino definition, tiles must be adjacent")
+            raise Exception("invalid domino 
+            definition, tiles must be adjacent")
         elif pos_1 in self.nodes or pos_2 in self.nodes:
             raise Exception(f"this place {(pos_1, pos_2)} is already taken")
         elif not self._is_not_overbounded_domino(pos_1, pos_2):
