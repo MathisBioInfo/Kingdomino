27b681dd3b39c1a339b6b7a17bf16f6ca2de8374
tyrosine-rex <thomas.lahaie@icloud.com>
2024-01-15 15:24:55 +0100

    color enum for pretty printing

---
+++ src/lib/colors.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class Colors(Enum):
+    RED = "\033[41m"
+    GREEN = "\033[42m"
+    YELLOW = "\033[43m"
+    BLUE = "\033[44m"
+    MAGENTA = "\033[45m"
+    CYAN = "\033[46m"
+    WHITE = "\033[47m"
+    NOTHING = "\033[0m"
+
+    def __call__(self, string):
+        return f"{self.value}{string}\033[0m"
