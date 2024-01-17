0ad286828f235ab3d518d931dd9566b7534fc80e
tyrosine-rex <thomas.lahaie@icloud.com>
2024-01-17 13:38:24 +0100

    some update

--- src/main.py
+++ src/main.py
@@ -1,24 +1,23 @@
 from lib.deck import Deck
-from lib.players import GreedyPlayer
+from lib.players import GreedyCompactPlayer, GameIsEnd
 from lib.dominos import Decor
 
 
 def main():
     deck = Deck()
-    ply_1 = GreedyPlayer("P1")
+    ply_1 = GreedyCompactPlayer("P1")
 
     while True:
         domino = deck.draw()
         try:
             ply_1.play(domino)
-        except Exception:
+        except GameIsEnd:
             print("Plus de place! Fin du Jeu")
             break
         else:
             print(ply_1, end="\n\n")
 
     Decor.legend()
-
 
 if __name__ == "__main__":
     main()
