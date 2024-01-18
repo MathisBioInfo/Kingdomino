1a29021811b4be418842f4fde44de6fa3960c054
MathisBioInfo <mathis.dupuy.etude@gmail.com>
2024-01-19 00:13:15 +0100

    improvement of the picking player system and first round function

--- src/lib/games.py
+++ src/lib/games.py
@@ -8,22 +8,14 @@ class GamesTwoPlayers:
         self.player_B = player_B
         self.deck = Deck()
         self.n_round = 0
-        self.reserved = {self.player_A:[],self.player_B:[]}
         self.reserved = []
     
     def start_game(self):
-        if random('A','B') == 'A':
-            first = self.player_A
-            second = self.player_B
-            third = self.player_B
-            fourth = self.player_A
-        else :
-            first = self.player_B
-            second = self.player_A
-            third = self.player_A
-            fourth = self.player_B
+        players = [self.player_A, self.player_B]
+        random.shuffle(players)
+        turn_order = players + players[::-1]  # [first, second, second, first]
         
-        self.first_round(first,second,third,fourth)
+        self.first_round(turn_order)
         self.n_round += 1
         for i in range(11):
             self.other_round()
@@ -31,13 +23,11 @@ class GamesTwoPlayers:
         self.last_round()
         self.final_score()
 
-    def first_round(self,first,second,third,fourth):
+    def first_round(self,turn_order):
         shop = self.deck.draw_n(4)
         self.reserved = []
-        self.reserved.append((first, first.pick_domino(shop)))
-        self.reserved.append((second, second.pick_domino(shop)))
-        self.reserved.append((third, third.pick_domino(shop)))
-        self.reserved.append((fourth, fourth.pick_domino(shop)))
+        for player in turn_order:
+            self.reserved.append((player, player.pick_domino(shop)))
 
     def other_round(self):
         shop = self.deck.draw_n(4)
@@ -51,7 +41,7 @@ class GamesTwoPlayers:
 
 
     def define_running_order(self):
-        running_order = sorted(self.reserved, lambda x: x[1][0].dom_id)
+        running_order = sorted(self.reserved, key=lambda x: x[1][0].dom_id)
         self.reserved = []
         return running_order
     
